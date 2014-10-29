83699461ec50f62b4e9f9aaccd8357cb53188374
mikedice <mikedice417@hotmail.com>
2014-10-29 08:05:13 -0700

    fix a bug

--- MyPhotos/MyPhotos/Controllers/HomeController.cs
+++ MyPhotos/MyPhotos/Controllers/HomeController.cs
@@ -22,70 +22,16 @@ namespace MyPhotos.Controllers
 
         public ActionResult Index()
         {
-            /*
-            var galleries = galleryManager.ListGalleries(GalleryOrder.LastUpdateDate, GalleryOrderDirection.Descending);
-
-
-            HomePageModel model = new HomePageModel();
-            var viewGalleries = new List<ViewGallery>();
-
-            var first = galleries.First();
-
-            int imageCount = first.Images.Count();
-            int numColumns = 0;
-            int imagesPerColumn = 0; 
-
-            if (imageCount <= MaxColumns)
-            {
-                imagesPerColumn = 1;
-                numColumns = imageCount;
-            }
-            else
-            {
-                numColumns = MaxColumns;
-                imagesPerColumn = imageCount / numColumns;
-            }
-
-            var images = first.Images.ToList();
-            List<Image>[] columns = new List<Image>[numColumns];
-            int imageIndex = 0;
-            int imagesRemain = images.Count;
-            for (int c = 0; c<columns.Length; c++)
-            {
-                columns[c] = new List<Image>();
-                int imagesToAdd = Math.Min(imagesRemain, imagesPerColumn);
-                for (int i =0; i<imagesToAdd; i++)
-                {
-                    columns[c].Add(images[imageIndex]);
-                    imageIndex++;
-                    imagesRemain--;
-                }
-            }
-
-            ViewGallery vg = new ViewGallery
-            {
-                ImageColumns = columns,
-                Gallery = first
-            };
-            viewGalleries.Add(vg);
-            model.ViewGalleries = viewGalleries;
-            return View(model);
-             */
             return View();
-            
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- MyPhotos/MyPhotos/Services/Image.cs
+++ MyPhotos/MyPhotos/Services/Image.cs
@@ -8,7 +8,6 @@ namespace MyPhotos.Services
     public class Image
     {
         public string Name { get; set; }
-        public string Caption { get; set; }
         public string ThumbnailUrl { get; set; }
         public string WebUrl { get; set; }
         public string MobileUrl { get; set; }
